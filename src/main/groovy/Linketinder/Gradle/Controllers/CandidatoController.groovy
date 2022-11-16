b9826b39bb26c98762cfd79c90c9053b43f215f6
HyagoRodrigues <hyago-rodrigues@live.com>
2022-11-16 12:58:41 -0300

    refact: Controllers

--- src/main/groovy/Linketinder/Gradle/Controllers/CandidatoController.groovy
+++ src/main/groovy/Linketinder/Gradle/Controllers/CandidatoController.groovy
@@ -1,15 +1,51 @@
 package Linketinder.Gradle.Controllers
 
+import Linketinder.Gradle.Model.Classes.PessoaFisica
 import Linketinder.Gradle.Model.DAO.CandidatoDAO
+import Linketinder.Gradle.Model.Metodos.ConectionFactory
+import Linketinder.Gradle.Model.Metodos.Utils
 
-class CandidatoController {
-    CandidatoDAO candidato = new CandidatoDAO();
+import javax.servlet.ServletException
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
-    void inserir_candidato(){
-        candidato.menu_candidato();
+
+@WebServlet(urlPatterns = "/candidatos")
+class CandidatoController extends HttpServlet {
+    CandidatoDAO candidatoDAO = new CandidatoDAO();
+    private ConectionFactory conexao = new ConectionFactory()
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException  {
+        PrintWriter out = res.getWriter();
+        def sql = conexao.conect();
+        sql.eachRow('SELECT * FROM candidatos') {
+            lista_candidato ->
+                out.println("${lista_candidato.nome}" + " ${lista_candidato.sobrenome}")
+
+        }
+        sql.close();
     }
 
-    void listar_candidatos(){
-        candidato.listar_candidatos()
+    @Override
+    void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PessoaFisica candidato = new PessoaFisica()
+        candidato.nome = req.getParameter("nome")
+        candidato.sobrenome = req.getParameter("sobrenome")
+        def data = req.getParameter("data_nascimento")
+        candidato.data_nascimento = Utils.formata_data(candidato.data_nascimento, data)
+        candidato.email = req.getParameter("email")
+        candidato.cpf = req.getParameter("cpf")
+        candidato.pais = req.getParameter("pais")
+        candidato.cep = req.getParameter("cep")
+        candidato.descricao = req.getParameter("descricao")
+        candidato.senha = req.getParameter("senha")
+
+        candidatoDAO.inserirCandidatoApi(candidato);
+
     }
+
+
 }
--- src/main/groovy/Linketinder/Gradle/Controllers/EmpresaController.groovy
+++ src/main/groovy/Linketinder/Gradle/Controllers/EmpresaController.groovy
@@ -1,15 +1,42 @@
 package Linketinder.Gradle.Controllers
 
+import Linketinder.Gradle.Model.Classes.PessoaJuridica
 import Linketinder.Gradle.Model.DAO.EmpresaDAO
+import Linketinder.Gradle.Model.Metodos.ConectionFactory
 
-class EmpresaController {
-    EmpresaDAO empresa = new EmpresaDAO();
+import javax.servlet.ServletException
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
-    void inserir_empresa(){
-        empresa.menu_empresa()
+@WebServlet(urlPatterns = "/empresas")
+class EmpresaController extends HttpServlet {
+    EmpresaDAO empresaDAO = new EmpresaDAO()
+    private ConectionFactory conexao = new ConectionFactory()
+
+    void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PessoaJuridica empresa = new PessoaJuridica();
+        empresa.nome = req.getParameter("nome")
+        empresa.cnpj = req.getParameter("cnpj")
+        empresa.email = req.getParameter("email")
+        empresa.descricao = req.getParameter("descricao")
+        empresa.pais = req.getParameter("pais")
+        empresa.cep = req.getParameter("cep")
+        empresa.senha= req.getParameter("senha")
+        empresaDAO.inserirEmpresaApi(empresa);
     }
 
-    void listar_empresas(){
-        empresa.listar_empresas()
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+        PrintWriter out = res.getWriter();
+        def sql = conexao.conect();
+        sql.eachRow('SELECT * FROM empresas') {
+            lista_competencias ->
+                out.println("${lista_competencias.nome}")
+
+        }
+        sql.close();
     }
 }
